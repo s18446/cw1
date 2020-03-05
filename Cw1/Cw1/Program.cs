dd07fa9cacf2a45289a2f4116722664058ef1990
s18446 <s18446@pjwstk.edu.pl>
2020-03-05 11:11:52 +0100

    Finish application

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+            //ThreadPool() - trzyma gotowe do dzialania watki 
+
+            if(result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+");
+                var matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+            
         }
     }
 }
